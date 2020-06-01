ee052d1caa6f04354fb6ed3eeb2f62cfc5e3aa8d
Khoi Nguyen <khoi@nguyen.me.uk>
2020-06-01 14:52:00 +0200

    [WEB] Put navbar in separate file

--- src/HomePage.vue
+++ src/HomePage.vue
@@ -1,34 +1,6 @@
 <template lang="pug">
 .container
-  nav.navbar
-    div.navbar-brand
-      a.navbar-item mathsnuggets
-    div.navbar-menu
-      div.navbar-start
-        a.navbar-item
-          span.icon
-            i.fas.fa-home
-          span Home
-        router-link(to="/slideshow_builder").navbar-item
-          span.icon
-            i.fas.fa-chalkboard-teacher
-          span Lesson Builder
-        div.navbar-item.has-dropdown.is-hoverable
-          a.navbar-link
-            span.icon
-              i.fas.fa-tools
-            span Utilities
-          div.navbar-dropdown
-            a.navbar-item Generator
-            a.navbar-item Solver
-            a.navbar-item Plotter
-        a.navbar-item About us
-      div.navbar-end
-        div.navbar-item
-          div.buttons
-            a.button.is-primary
-              strong Signup
-            a.button.is-light Log in
+  NavBar
   .columns
     .column(v-for="card in cards")
       div.feature.has-text-centered
@@ -41,6 +13,7 @@
 <script>
 import 'typeface-fira-sans'
 import 'bulma/css/bulma.css'
+import NavBar from './NavBar'
 
 export default {
   name: 'HomePage',
@@ -59,16 +32,14 @@ export default {
         }
       ]
     }
+  },
+  components: {
+    NavBar
   }
 }
 </script>
 
-<style>
-.navbar-brand .navbar-item {
-  color: black;
-  font-family: "Fira Sans";
-  font-size: 1.5em;
-}
+<style scoped>
 .feature, .feature .title {
   color: #414141;
   font-family: "Fira Sans";
