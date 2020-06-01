70e7cb069176c6ab712362baf238be801fa45f4c
Khoi Nguyen <khoi@nguyen.me.uk>
2020-06-01 14:39:49 +0200

    [WEB] Add features on home page

--- src/HomePage.vue
+++ src/HomePage.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-div
+.container
   nav.navbar
     div.navbar-brand
       a.navbar-item mathsnuggets
@@ -29,6 +29,13 @@ div
             a.button.is-primary
               strong Signup
             a.button.is-light Log in
+  .columns
+    .column(v-for="card in cards")
+      div.feature.has-text-centered
+        span.card-header-icon.is-large.is-left
+          i.fa-3x(:class="card.icon")
+        h3.title.is-3.has-text-centered {{ card.title }}
+        .content.is-size-5 {{ card.text }}
 </template>
 
 <script>
@@ -36,7 +43,23 @@ import 'typeface-fira-sans'
 import 'bulma/css/bulma.css'
 
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  data () {
+    return {
+      cards: [
+        {
+          icon: 'fas fa-chalkboard-teacher',
+          title: 'Lesson builder',
+          text: 'Construct and save your own slideshow lessons using: Generator, Solver and Plotter and all other basic slideshow creating tools.'
+        },
+        {
+          icon: 'fas fa-calculator',
+          title: 'Solver',
+          text: 'Construct and save your own slideshow lessons using: Generator, Solver and Plotter and all other basic slideshow creating tools.'
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -45,5 +68,13 @@ export default {
   color: black;
   font-family: "Fira Sans";
   font-size: 1.5em;
+}
+.feature, .feature .title {
+  color: #414141;
+  font-family: "Fira Sans";
+  font-weight: 300;
+}
+.feature .content {
+  line-height: 1.4;
 }
 </style>
