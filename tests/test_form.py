f2c4782933fb83237cbbe74c6714550efabe4a6b
Khoi Nguyen <khoi@nguyen.me.uk>
2020-06-03 11:56:16 +0200

    [TESTS/FORM] Improve tests

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,47 +1,45 @@
+import sympy
+
 from mathsnuggets.core import fields, form
 
 
 class CustomForm(form.Form):
-    name = "foobar"
-    equation = fields.Equation("Equation")
-
-    first = fields.Field("First", order="_")
-    alpha = fields.Field("Starts with A")
-    last = fields.Field("Last", order="z")
-
     template = "Solve `equation`"
-
+    equation = fields.Equation("Equation", default="x^3")
     required = fields.Field("Required field", required=True)
-    required_2 = fields.Field("Required field", required=True)
 
     def validate(self):
         if str(self.equation) != "Eq(x**2, 0)":
             raise AttributeError("Wrong equation")
 
-    def solve(self):
-        return "Solution"
-
-    public = "Public"
-    _private = "Should not be exported"
-
 
 form = CustomForm()
 
 
+def test_default_values():
+    form = CustomForm(required="Hello")
+    assert form.required == "Hello"
+
+    with sympy.evaluate(False):
+        assert form.equation == sympy.Eq(sympy.Symbol("x") ** 3, 0)
+
+
 def test_validate():
     form.required = "Hello"
-
-    assert form.required_2 is None
-    assert not form.valid
-
-    form.required_2 = "Hello"
     assert not form.valid
 
     form.equation = "x^2"
     assert form.valid
 
+    form.required = None
+    assert not form.valid
+    form.required = "Hello"
+
 
 def test_export():
     assert form.valid
     export = dict(form._fields())
-    assert "_private" not in export.keys()
+    assert len(export) == 2
+    assert export["equation"]["before"] == "Solve "
+    assert export["equation"]["order"] == 0
+    assert "value" in export["equation"]
