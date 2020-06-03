9daa04d9c62de497c1e41056e0d3f322969dcac7
Khoi Nguyen <khoi@nguyen.me.uk>
2020-06-04 00:36:00 +0200

    [WEB] Add my profile

--- src/About.vue
+++ src/About.vue
@@ -10,7 +10,27 @@ div
   .container
     .columns
       .column.has-text-centered
-        h3.title.is-3 Khoi Nguyen
+        h3.title.is-3 Khôi Nguyen
+        h4.subtitle.is-5 Full Stack Developer
+        p.content.
+          Hello! My name is Khoi and I am the main developer on this project.
+          My responsibility is to implement new features
+          and ensure the software works well.
+        p.content.
+          I am a pure mathematician by education.
+          I got my MMath from the
+          #[a(href="https://cam.ac.uk") University of Cambridge] in 2012,
+          followed by a PhD from
+          #[a(href="https://imperial.ac.uk") Imperial College] in 2016.
+          I started teaching shortly afterwards,
+          and obtained by PGCE from the
+          #[a(href="https://ox.ac.uk") University of Oxford] in 2020.
+          I now work as a Teacher of Mathematics at the
+          #[a(href="https://kingscollegeschools.org/madrid/school/") British School of Madrid].
+        p.content.
+          Outside of teaching, I enjoy playing piano and table tennis.
+          I represented Oxford's first team in the 77#[sup th]
+          Table Tennis Varsity match against Cambridge.
       .column.has-text-centered
         h3.title.is-3 Tom Chambaud
 </template>
