6f2d40daed63de6715e2706d1504845bc8dec561
Thomas Chambaud-Boudet <thomaschambaud@gmail.com>
2020-06-15 13:57:46 +0100

    [WIDGETS] Create position in sequence solver

---
+++ mathsnuggets/widgets/sequence_position.py
@@ -0,0 +1,30 @@
+import sympy
+
+from mathsnuggets.core import fields, form
+from mathsnuggets.widgets import sequence_term
+
+
+class SequencePosition(form.Form):
+    """Position in Sequence"""
+
+    template = "Find the position of `element` in `sequence` `position`"
+
+    sequence = fields.Expression("Sequence", required=True)
+    element = fields.Expression("term", required=True)
+
+    @fields.computed("Find position")
+    def position(self):
+        # determine if sequence has attribute free_symbols and check if sequence has "n"
+        if hasattr(self.sequence, "free_symbols") and sympy.symbols("n") in self.sequence.free_symbols:
+            general_term = self.sequence
+        # solve for the general term
+        else:
+            general_term = sequence_term.SequenceTerm(sequence=str(self.sequence)).term.args[1]
+        # equate element to general term
+        equation = sympy.Eq(self.element, general_term)
+        # solve the equation
+        solution_set = sympy.solveset(equation, sympy.symbols("n"), sympy.S.Naturals0)
+        # solution set is natural numbers or one element
+        if solution_set == sympy.S.EmptySet:
+            raise ValueError("The 'term' you have entered is not in the sequence you have given")
+        return solution_set
