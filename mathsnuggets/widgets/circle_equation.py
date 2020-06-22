a16ce8724e6fbb99d19793d24c464ecf896d4c49
Thomas Chambaud-Boudet <thomaschambaud@gmail.com>
2020-06-22 12:29:28 +0100

    [WIDGETS/circle_equations] Create solver

---
+++ mathsnuggets/widgets/circle_equation.py
@@ -0,0 +1,26 @@
+import sympy
+
+from mathsnuggets.core import fields, form
+
+
+class CircleEquation(form.Form):
+    """Circle radius and center"""
+
+    template = "Find the `info` of the circle `equation` `solution`"
+
+    equation = fields.Equation("Equation", required=True)
+    info = fields.Select("Info", options=["radius", "center"])
+
+    @fields.computed("Radius/Center")
+    def solution(self):
+        (a, h, k, x, y), r = (
+            sympy.symbols("a h k x y"),
+            sympy.symbols("r", positive=True),
+        )
+        formula = a * ((x - h) ** 2 + (y - k) ** 2 - r ** 2)
+        equation = sympy.expand(self.equation.args[0] - self.equation.args[1] - formula)
+        system = [equation.coeff(*t) for t in [(x, 2), (y, 2), (x, 1), (y, 1), (x, 0)]]
+        values = sympy.solve(system, (a, r, h, k), dict=True)[0]
+        if not values:
+            raise ValueError("The 'equation' you have entered is not that of a circle")
+        return values[r] if self.info == "radius" else (values[h], values[k])
