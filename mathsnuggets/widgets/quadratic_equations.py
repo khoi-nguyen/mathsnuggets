91736bb79fc58db883cb92f4db869d2d18530758
Thomas Chambaud-Boudet <thomaschambaud@gmail.com>
2020-06-23 10:50:39 +0100

    [WIDGETS/quadratic_equations] create solver and basic generator

---
+++ mathsnuggets/widgets/quadratic_equations.py
@@ -0,0 +1,19 @@
+import sympy
+
+from mathsnuggets.core import fields
+from mathsnuggets.widgets import equation
+
+
+class QuadraticEquations(equation.Equation):
+    """Quadratic Equations"""
+
+    a = fields.RandomNumber("a")
+    b = fields.RandomNumber("b")
+    c = fields.RandomNumber("c")
+
+    def generator(self):
+        self.equation = sympy.Eq(self.a * self.x ** 2 + self.b * self.x + self.c)
+
+    @fields.range_constraint("Quadratic", default=True, hidden=True, protected=True)
+    def quadratic(self):
+        self.a -= {0}
