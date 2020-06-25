bb514514a6cd9395faa6cac2ec85281d330ce203
Thomas Chambaud-Boudet <thomaschambaud@gmail.com>
2020-06-25 09:41:31 +0100

    [WIDGETS/fraction_operations] Add generator

--- mathsnuggets/widgets/fraction_operations.py
+++ mathsnuggets/widgets/fraction_operations.py
@@ -21,3 +21,25 @@ class FractionOperations(form.Form):
         else:
             answer = self.fraction_1 / self.fraction_2
         return answer
+
+    a = fields.RandomNumber("a")
+    b = fields.RandomNumber("b")
+    c = fields.RandomNumber("c")
+    d = fields.RandomNumber("d")
+
+    def generator(self):
+        self.fraction_1 = self.a / self.b
+        self.fraction_2 = self.c / self.d
+
+    @fields.constraint("t")
+    def true(self):
+        return True
+
+    @fields.range_constraint(
+        "non-zero numbers", default=True, hidden=True, protected=True
+    )
+    def non_zero_numbers(self):
+        self.a -= {0}
+        self.b -= {0}
+        self.c -= {0}
+        self.d -= {0}
