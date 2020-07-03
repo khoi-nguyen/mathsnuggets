42fddf318676111e49fccbf18c1389c653145cc1
Thomas Chambaud-Boudet <thomaschambaud@gmail.com>
2020-07-03 09:24:45 +0100

    [WIDGETS/quadratic_equations] Change representation of quadratic equation

--- mathsnuggets/widgets/quadratic_equations.py
+++ mathsnuggets/widgets/quadratic_equations.py
@@ -10,12 +10,16 @@ class QuadraticEquations(equation.Equation):
     """Quadratic Equations"""
 
     a = fields.RandomNumber("a")
-    b = fields.RandomNumber("b")
-    c = fields.RandomNumber("c")
+    x1 = fields.RandomNumber("x1")
+    x2 = fields.RandomNumber("x2")
 
     def generator(self):
-        self.equation = sympy.Eq(self.a * self.x ** 2 + self.b * self.x + self.c)
+        self.equation = sympy.expand(
+            sympy.Eq(self.a * (self.x - self.x1) * (self.x - self.x2))
+        )
 
-    @fields.range_constraint("Quadratic", default=True, hidden=True, protected=True)
-    def quadratic(self):
+    @fields.range_constraint(
+        "Non-zero expression", default=True, hidden=True, protected=True
+    )
+    def nonzero_expression(self):
         self.a -= {0}
