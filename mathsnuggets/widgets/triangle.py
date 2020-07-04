a9cdc4dbe555a035ed2e2b6281b8d4305389aebc
Khoi Nguyen <khoi@nguyen.me.uk>
2020-07-04 16:37:30 +0200

    [WIDGETS/Triangle] Vertices from lengths & angles

---
+++ mathsnuggets/widgets/triangle.py
@@ -0,0 +1,70 @@
+import sympy
+
+from mathsnuggets.core import fields, form
+
+
+class Triangle(form.Form):
+    """Draw a triangle"""
+
+    template = """
+        Vertices labels: `A`, `B`, `C`<br>
+        Lengths: `a`, `b`, `c`<br>
+        Angles: `alpha`, `beta`, `gamma`<br>
+    """
+
+    A = fields.Field("A", default="A")
+    B = fields.Field("B", default="B")
+    C = fields.Field("C", default="C")
+
+    a = fields.Expression("a")
+    b = fields.Expression("b")
+    c = fields.Expression("c")
+
+    alpha = fields.Expression("alpha")
+    beta = fields.Expression("beta")
+    gamma = fields.Expression("gamma")
+
+    @fields.computed("Vertices")
+    def vertices(self):
+        lengths = [self.a, self.b, self.c]
+        angles = [self.alpha, self.beta, self.gamma]
+        missing_quantities = (lengths + angles).count(None)
+
+        def cosine_law(lengths, angles, index):
+            (c, a, b), gamma = lengths[index:] + lengths[:index], angles[index]
+            x, find_angle = sympy.Dummy("x"), not bool(gamma)
+            gamma, c = gamma or x, c or x
+            equation = sympy.Eq(c ** 2, a ** 2 + b ** 2 - 2 * a * b * sympy.cos(gamma))
+            domain = sympy.Interval.open(0, sympy.pi if find_angle else sympy.oo)
+            sol = [x for x in sympy.solve(equation) if x in domain][0]
+            locals()["angles" if find_angle else "lengths"][index] = sol
+
+        def sine_law(lengths, angles, i, j):
+            a, b, alpha, beta = lengths[i], lengths[j], angles[i], angles[j]
+            x, find_angle = sympy.Dummy("x"), not bool(beta)
+            b, beta = b or x, beta or x
+            equation = sympy.Eq(sympy.sin(alpha) / a, sympy.sin(beta) / b)
+            # TODO: ambiguity with sine law
+            domain = sympy.Interval.open(0, sympy.pi / 2 if find_angle else sympy.oo)
+            sol = [x for x in sympy.solve(equation) if x in domain][0]
+            locals()["angles" if find_angle else "lengths"][j] = sol
+
+        while missing_quantities:
+            missing_info = [[lengths[i], angles[i]].count(None) for i in range(3)]
+            if angles.count(None) == 1:
+                angles[angles.index(None)] = sympy.pi - sum([a for a in angles if a])
+            elif lengths.count(None) == 0 and angles.count(None):
+                cosine_law(lengths, angles, angles.index(None))
+            elif lengths.count(None) == 1 and angles[lengths.index(None)]:
+                cosine_law(lengths, angles, lengths.index(None))
+            elif {0, 1} <= set(missing_info):
+                sine_law(lengths, angles, missing_info.index(0), missing_info.index(1))
+            else:
+                raise ValueError("Not enough information to find the vertices")
+            missing_quantities -= 1
+
+        return [
+            (0, 0),
+            (lengths[0], 0),
+            (lengths[2] * sympy.cos(angles[1]), lengths[2] * sympy.sin(angles[1])),
+        ]
