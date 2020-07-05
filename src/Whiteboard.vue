9c59b7317e490bb230e3a878534bb6e3d9df1d6a
Khoi Nguyen <khoi@nguyen.me.uk>
2020-07-05 02:10:33 +0200

    [WEB] Basic whiteboard component

---
+++ src/Whiteboard.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+div
+  canvas#test(width="600" height="400")
+</template>
+
+<script>
+import { fabric } from 'fabric'
+
+export default {
+  data () {
+    return {
+      canvas: false
+    }
+  },
+  mounted () {
+    this.canvas = new fabric.Canvas('test')
+    this.canvas.isDrawingMode = true
+    this.canvas.freeDrawingBrush.width = 3
+  }
+}
+</script>
+
+<style scoped>
+canvas {
+  border: 1px solid black;
+}
+</style>
