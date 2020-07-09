d8f5b15451dac89d3f318bdd047ffe0ec94abfe5
Khoi Nguyen <khoi@nguyen.me.uk>
2020-07-09 20:41:15 +0200

    [TEST] Test generator

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -10,3 +10,24 @@ def test_widgets():
         for _name, field in form._fields():
             if field.get("computed") or field.get("required"):
                 assert field.get("value") or field.get("html")
+
+
+def test_generators():
+    widget_names = [n for n in dir(widgets) if n[0].isupper() and n[1].islower()]
+    for widget in widget_names:
+        form = getattr(widgets, widget)()
+        if not hasattr(form, "generator"):
+            continue
+
+        # Without constraints
+        form.generate()
+        assert form.valid
+
+        for name, _ in form._fields(
+            lambda f: f.get("constraint") and not f.get("protected")
+        ):
+            payload = {}
+            payload[name] = True
+            form = getattr(widgets, widget)(**payload)
+            form.generate()
+            assert form.valid
