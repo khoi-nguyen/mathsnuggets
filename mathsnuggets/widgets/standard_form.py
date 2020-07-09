ecf2488a5c5476a87e1c3629cc1e675a82854dfa
Thomas Chambaud-Boudet <thomaschambaud@gmail.com>
2020-07-09 18:47:56 +0100

    [WIDGETS/standard_form] Create generator

--- mathsnuggets/widgets/standard_form.py
+++ mathsnuggets/widgets/standard_form.py
@@ -10,11 +10,13 @@ class StandardForm(form.Form):
 
     index_1 = fields.StandardForm("index 1")
     index_2 = fields.StandardForm("index 2")
-    operation = fields.Select("operation", options=["×", "÷"])
+    operation = fields.Select("operation", options=["+", "-", "×", "÷"])
 
     @fields.computed("Solution", field=fields.StandardForm)
     def solution(self):
         operations = {
+            "+": sympy.Add,
+            "-": lambda a, b: a - b,
             "×": sympy.Mul,
             "÷": lambda a, b: a / b,
         }
@@ -22,3 +24,18 @@ class StandardForm(form.Form):
             return sympy.simplify(
                 operations[self.operation](self.index_1, self.index_2)
             )
+
+    a = fields.RandomNumber("a")
+    b = fields.RandomNumber("b")
+    m = fields.RandomNumber("m")
+    n = fields.RandomNumber("n")
+    op = fields.RandomNumber("op", default=["+", "-", "×", "÷"])
+
+    def generator(self):
+        self.index_1 = self.a * 10 ** self.m
+        self.index_2 = self.b * 10 ** self.n
+        self.operation = self.op
+
+    @fields.constraint("Positive powers")
+    def positive(self):
+        return self.m > 0 and self.n > 0
