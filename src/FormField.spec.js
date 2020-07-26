f7fd51ac417b4e56f5cd89446168fbe10f97f767
Khoi Nguyen <khoi@nguyen.me.uk>
2020-07-27 01:59:49 +0200

    [UNIT] Test expression fields

--- src/FormField.spec.js
+++ src/FormField.spec.js
@@ -1,14 +1,48 @@
-import { shallowMount } from '@vue/test-utils'
+import 'regenerator-runtime/runtime'
 import FormField from './FormField.vue'
+import flushPromises from 'flush-promises'
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { enableFetchMocks } from 'jest-fetch-mock'
+import AsyncComputed from 'vue-async-computed'
+
+enableFetchMocks()
+
+const localVue = createLocalVue()
+localVue.use(AsyncComputed)
 
 let wrapper = shallowMount(FormField)
 
 describe('FormField', () => {
   beforeEach(() => {
     wrapper = shallowMount(FormField)
+    fetch.resetMocks()
   })
 
   it('is called', () => {
     expect(wrapper).toBeTruthy()
+  })
+
+  it('expression field without default value', async () => {
+    fetch.mockResponses(JSON.stringify({
+      latex: '\\frac {1}{x}',
+      valid: true,
+      value: '1/x'
+    }))
+    wrapper = shallowMount(FormField, {
+      localVue,
+      propsData: {
+        label: 'Field',
+        type: 'Expression'
+      }
+    })
+
+    expect(wrapper.find('textarea')).toBeTruthy()
+    expect(wrapper.html()).toContain('Field')
+
+    wrapper.vm.value = '1/x'
+    await flushPromises()
+    expect(wrapper.contains('textarea')).toBeFalsy()
+    expect(wrapper.html()).toContain('katex')
+    expect(fetch).toHaveBeenCalledTimes(1)
   })
 })
