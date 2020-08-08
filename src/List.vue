fdb95045651e1812b16efdb5a16352c58d3bbaa5
Khoi Nguyen <khoi@nguyen.me.uk>
2020-08-08 22:19:17 +0200

    [WEB/List] Placeholder for empty list

--- src/List.vue
+++ src/List.vue
@@ -1,11 +1,21 @@
 <template lang="pug">
 .content.is-size-3
   component(:is="payload.numbered ? 'ol' : 'ul'")
+    li.placeholder(v-if="!children.length") Add an item to this list
     slot
 </template>
 
 <script>
 export default {
-  props: { payload: { type: Object, default: () => {} } }
+  props: {
+    children: [],
+    payload: { type: Object, default: () => {} }
+  }
 }
 </script>
+
+<style scoped>
+.placeholder {
+  color: #aa7582;
+}
+</style>
