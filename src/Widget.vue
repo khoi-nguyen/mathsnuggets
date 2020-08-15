0c45eac86540281c05b9d9f2c4e19ca495a9f1da
Khoi Nguyen <khoi@nguyen.me.uk>
2020-08-15 13:26:40 +0200

    [WEB] Use a watcher for the solver

--- src/Widget.vue
+++ src/Widget.vue
@@ -9,7 +9,7 @@ form.is-size-3.avoid-column
 </template>
 
 <script>
-import { isEmpty, filter, forEach, pickBy } from 'lodash'
+import { isEmpty, isEqual, filter, forEach, pickBy } from 'lodash'
 import VRuntimeTemplate from 'v-runtime-template'
 
 import api from './ajax'
@@ -22,30 +22,53 @@ export default {
     type: String
   },
   data () {
-    return { error: {} }
+    return {
+      computed: {},
+      error: {}
+    }
+  },
+  computed: {
+    solverPayload () {
+      return pickBy(this.payload, (value, fieldName) => {
+        const field = this.widgetData.fields[fieldName]
+        return !field.random && !field.constraint
+      })
+    },
+    generatorPayload () {
+      return pickBy(this.payload, (value, fieldName) => {
+        const field = this.widgetData.fields[fieldName]
+        return field.random || field.constraint
+      })
+    }
+  },
+  methods: {
+    async solve (generator = false) {
+      const method = generator ? 'POST' : 'GET'
+      const payload = generator ? this.generatorPayload : this.solverPayload
+      const data = await api(`widgets/${this.type}`, method, payload)
+      this.error = data.error ? data : {}
+      forEach(data, (value, fieldName) => {
+        const payload = this.widgetData.fields[fieldName].computed ? this.computed : this.payload
+        if (payload[fieldName] !== value) {
+          this.$set(payload, fieldName, value)
+        }
+      })
+    }
+  },
+  watch: {
+    solverPayload: {
+      handler (newValue, oldValue) {
+        const isValid = !isEmpty(this.widgetData.fields) && isEmpty(filter(this.widgetData.fields, f => {
+          return f.required && !f.default && !this.payload[f.name]
+        }))
+        if (isValid && !isEqual(newValue, oldValue)) {
+          this.solve()
+        }
+      },
+      deep: true
+    }
   },
   asyncComputed: {
-    computed: {
-      async get () {
-        const computed = {}
-        const data = await api(`widgets/${this.type}`, 'GET', pickBy(this.payload, (value, fieldName) => {
-          const field = this.widgetData.fields[fieldName]
-          return !field.random && !field.constraint
-        }))
-        this.error = data.error ? data : {}
-        forEach(data, (value, fieldName) => {
-          const payload = this.widgetData.fields[fieldName].computed ? computed : this.payload
-          this.$set(payload, fieldName, value)
-        })
-        return computed
-      },
-      default: {},
-      shouldUpdate () {
-        return !isEmpty(this.widgetData.fields) && isEmpty(filter(this.widgetData.fields, f => {
-          return f.required && !f.default && !(f.name in this.payload)
-        }))
-      }
-    },
     widgetData: {
       async get () {
         return await api(`widgets/${this.type}`)
