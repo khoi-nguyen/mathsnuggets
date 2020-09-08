93c44fed4073c781be7329deebe15e9a39f6f4c6
Khoi Nguyen <khoi@nguyen.me.uk>
2020-09-08 16:21:37 +0200

    [API] Add Geogebra widget

---
+++ mathsnuggets/widgets/geogebra.py
@@ -0,0 +1,32 @@
+"""
+=======
+Youtube
+=======
+"""
+from mathsnuggets.core import fields, form
+
+test = {"url": "https://www.geogebra.org/m/UjjwuM8p"}
+
+
+class Geogebra(form.Form):
+    """Geogebra"""
+
+    name = "Geogebra"
+    url = fields.Field("URL")
+    width = fields.Field("Width", default=800)
+    height = fields.Field("Width", default=600)
+    template = """`url` (`width`x`height`) `geogebra`"""
+
+    @fields.computed("Geogebra", field=fields.Html)
+    def geogebra(self):
+        """Get HTML code of embedded YouTube video"""
+        url = self.url.split("/")[-1]
+        url = f"https://www.geogebra.org/material/iframe/id/{url}/width/{self.width}/height/{self.height}"
+        return f"""
+          <iframe scrolling="no"
+            src="{url}"
+            height="{self.height}"
+            width="{self.width}"
+            style="border: 0px;" allowfullscreen>
+          </iframe>
+        """
