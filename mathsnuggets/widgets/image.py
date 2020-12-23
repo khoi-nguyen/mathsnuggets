c0ebfde6844c52647af4fe1d0acff43682c5e375
Khoi Nguyen <khoi@nguyen.me.uk>
2020-12-23 11:52:04 +0100

    [WIDGETS] Write image to permanent storage

--- mathsnuggets/widgets/image.py
+++ mathsnuggets/widgets/image.py
@@ -1,3 +1,7 @@
+import base64
+import hashlib
+import mimetypes
+
 from mathsnuggets.core import fields, form
 
 
@@ -10,3 +14,13 @@ class Image(form.Form):
     @fields.computed("Image", field=fields.Html, nohide=True)
     def image(self):
         return f"""<img src="{self.src}" />"""
+
+    def validate(self):
+        if "," in self.src:
+            data = self.src.split(",")
+            image_hash = hashlib.sha1(data[1].encode("utf-8")).hexdigest()
+            ext = mimetypes.guess_extension(data[0][5 : data[0].find(";")])
+            filename = f"storage/{image_hash}{ext}"
+            with open(filename, "wb") as f:
+                f.write(base64.b64decode(data[1]))
+            self.src = "/" + filename
