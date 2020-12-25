12066e62a9d04820bdbe8443664f957d03c57ed3
Khoi Nguyen <khoi@nguyen.me.uk>
2020-12-25 13:52:57 +0100

    [WIDGETS] Factorisation question

---
+++ mathsnuggets/widgets/factorise_question.py
@@ -0,0 +1,42 @@
+import uuid
+
+import sympy
+
+from mathsnuggets.core import fields, form
+
+
+class FactoriseQuestion(form.Form):
+    """Factorisation Question"""
+
+    name = fields.Field("Survey name")
+    answer = fields.Expression("Your answer", nosave=True)
+    expression = fields.Expression("Expression")
+
+    template = """
+        <p>Factorise `expression`</p>
+        <survey
+            :name="payload.name"
+            :showStats="config.authState.loggedIn"
+            :correct="computed.correct"
+            :value="payload.answer">
+            `answer`
+        </survey>
+    """
+
+    @fields.computed("Correct", field=fields.Boolean)
+    def correct(self):
+        if (
+            not self.answer
+            or sympy.simplify(self.answer - self.expression) != 0
+            or self.answer.func != sympy.Mul
+        ):
+            return False
+        for term in self.answer.args:
+            if sympy.factor(term).func == sympy.Mul:
+                return False
+        return True
+
+    def validate(self):
+        self.expression = sympy.expand(self.expression)
+        if not self.name:
+            self.name = str(uuid.uuid1())
