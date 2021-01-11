101892481a9f5be7704e0dca2d2d70a99a5284fa
Khoi Nguyen <khoi@nguyen.me.uk>
2021-01-11 21:44:26 +0100

    [WIDGETS] Don't hide youtube video

--- mathsnuggets/widgets/youtube.py
+++ mathsnuggets/widgets/youtube.py
@@ -15,7 +15,7 @@ class Youtube(form.Form):
     url = fields.Field("URL")
     template = """`url` `video`"""
 
-    @fields.computed("Youtube Video", field=fields.Html)
+    @fields.computed("Youtube Video", field=fields.Html, nohide=True)
     def video(self):
         """Get HTML code of embedded YouTube video"""
         url = self.url.replace("watch?v=", "embed/")
