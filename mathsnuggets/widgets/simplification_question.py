edc147f32af9ea8bcb0166765526cf9e72e703ce
Khoi Nguyen <khoi@nguyen.me.uk>
2021-01-11 22:51:40 +0100

    [WIDGETS] Add simplification survey widget

---
+++ mathsnuggets/widgets/simplification_question.py
@@ -0,0 +1,34 @@
+import uuid
+
+import sympy
+
+from mathsnuggets.core import fields, form
+
+
+class SimplificationQuestion(form.Form):
+    """Simplification Question"""
+
+    name = fields.Field("Survey name")
+    answer = fields.Expression("Your answer", nosave=True, editable=True)
+    expression = fields.Expression("Expression")
+
+    template = """
+        <p>Simplify `expression`</p>
+        <survey
+            :name="payload.name"
+            :showStats="config.authState.loggedIn"
+            :correct="computed.correct"
+            :value="payload.answer">
+            `answer`
+        </survey>
+    """
+
+    @fields.computed("Correct", field=fields.Boolean)
+    def correct(self):
+        if not self.answer:
+            return False
+        return self.answer == sympy.simplify(self.expression)
+
+    def validate(self):
+        if not self.name:
+            self.name = str(uuid.uuid1())
