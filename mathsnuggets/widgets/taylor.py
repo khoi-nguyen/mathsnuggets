3bbca433be57a278da8873e1608d20cee35fea11
Batu Engin <enginbatu@gmail.com>
2021-02-15 15:43:46 +0100

    Fixed Changes

--- mathsnuggets/widgets/taylor.py
+++ mathsnuggets/widgets/taylor.py
@@ -8,13 +8,12 @@ test = {"function": "x sin x"}
 class Taylor(form.Form):
     """Taylor Expansion"""
 
-    template = """
-            Expand `expression``expand`
-    """
+    template = """Expand `expression``expand`"""
 
     expression = fields.Expression("Expression")
     x = fields.Expression("x", default="x")
+    order = 6
 
     @fields.computed("Taylor")
     def taylor(self):
-        return sympy.series(self.expression)
+        return sympy.series(self.expression, x=self.x, x0=self.x0, n=self.order)
