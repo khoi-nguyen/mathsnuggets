f87abe4b46ebbfd5d442aa60bce847d2ad6ad56e
Khoi Nguyen <khoi@nguyen.me.uk>
2021-02-15 15:43:09 +0100

    [WEB] Env not editable unless they need to be

--- src/Environment.vue
+++ src/Environment.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .container.avoid-column
   b-collapse.message.is-medium(:class="`is-${payload.style || 'primary'}`" :open="!payload.collapsed")
-    h3.message-header(contenteditable @blur="blur" @keydown.enter.prevent="blur" v-text="payload.title" slot="trigger")
+    h3.message-header(:contenteditable="config.edit" @blur="blur" @keydown.enter.prevent="blur" v-text="payload.title" slot="trigger")
     article
       .message-body
         slot
@@ -9,7 +9,7 @@
 
 <script>
 export default {
-  props: { payload: { type: Object, default: () => {} } },
+  props: { config: Object, payload: { type: Object, default: () => {} } },
   methods: {
     blur (event) {
       this.updateTitle(event.target.innerText)
