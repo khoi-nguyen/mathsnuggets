eb56016f1b7d04a96ddeabdcf80e321d08420874
Khoi Nguyen <khoi@nguyen.me.uk>
2021-02-17 22:01:53 +0100

    [WIDGETS/Taylor] Change template

--- mathsnuggets/widgets/taylor.py
+++ mathsnuggets/widgets/taylor.py
@@ -2,16 +2,25 @@ import sympy
 
 from mathsnuggets.core import fields, form
 
+
 class Taylor(form.Form):
     """Taylor Expansion"""
 
-    template = """Taylor expansion of `expression` of order `order` with respect to `x` around `x0`"""
+    template = """
+        Taylor expansion of `expression`
+        of order `order`
+        <span v-if="config.edit || payload.x != 'x'">
+            with respect to `x`
+        </span>
+        around `x0`
+        `taylor`
+    """
 
     expression = fields.Expression("Expression")
     x = fields.Expression("x", default="x")
     order = fields.Expression("Order", default=6)
     x0 = fields.Expression("x0", default=0)
 
-    @fields.computed("Taylor")
+    @fields.computed("Taylor expansion")
     def taylor(self):
         return sympy.series(self.expression, x=self.x, x0=self.x0, n=self.order)
