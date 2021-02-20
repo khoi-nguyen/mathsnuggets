593cf07d6e6477be38f92644bafd046afc850bda
Khoi Nguyen <khoi@nguyen.me.uk>
2021-02-20 04:14:04 +0100

    [WIDGETS/Youtube] Allow video resizing

--- mathsnuggets/widgets/youtube.py
+++ mathsnuggets/widgets/youtube.py
@@ -13,19 +13,30 @@ class Youtube(form.Form):
 
     name = "YouTube"
     url = fields.Field("URL")
+    zoom = fields.Expression("Zoom", default="1.0")
     template = """
         <div v-if="config.edit">
-            `url`
+            URL: `url`
+            Zoom: `zoom`
         </div>
         `video`
     """
+
+    @property
+    def height(self):
+        return 315 * self.zoom
+
+    @property
+    def width(self):
+        return 560 * self.zoom
 
     @fields.computed("Youtube Video", field=fields.Html, nohide=True)
     def video(self):
         """Get HTML code of embedded YouTube video"""
         url = self.url.replace("watch?v=", "embed/")
         return f"""
-            <iframe width="560" height="315" src="{url}" frameborder="0"
+            <iframe width="{self.width}" height="{self.height}"
+                src="{url}" frameborder="0"
                 allow="autoplay; encrypted-media" allowfullscreen>
             </iframe>
         """
