34f2e62744b6019ffaf396fe7ee32fcd337646b2
Khoi Nguyen <khoi@nguyen.me.uk>
2021-03-01 01:09:18 +0100

    [WIDGETS/Euler] Support midpoint method

--- mathsnuggets/widgets/euler_method.py
+++ mathsnuggets/widgets/euler_method.py
@@ -11,6 +11,7 @@ class EulerMethod(form.Form):
             <li>Initial condition: `x0`, `y0`</li>
             <li><span class="katex">h = </span> `h`</li>
             <li><span class="katex">n = </span> `n`</li>
+            <li>`midpoint`</li>
         </ul>
         `solution`
     """
@@ -20,11 +21,17 @@ class EulerMethod(form.Form):
     y0 = fields.Expression("Initial condition (y)", required=True)
     h = fields.Expression("Step length", default="0.1")
     n = fields.Expression("Number of iterations", default="3")
+    midpoint = fields.Boolean("Use midpoint method")
 
     @fields.computed("Solution")
     def solution(self):
-        x, y = self.x0, self.y0
+        x, y, old_y = self.x0, self.y0, self.y0
         for k in range(self.n):
-            y += self.function.subs({"x": x, "y": y}) * self.h
+            grad = self.function.subs({"x": x, "y": y})
+            if self.midpoint and k > 0:
+                new_y = old_y + grad * 2 * self.h
+                old_y, y = y, new_y
+            else:
+                y += grad * self.h
             x += self.h
         return (x, y)
