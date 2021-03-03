92f79853073903343f9ed0f6b1681012c0cb8581
Khoi Nguyen <khoi@nguyen.me.uk>
2021-03-03 11:18:47 +0100

    [WIDGETS] Add simplify widget

---
+++ mathsnuggets/widgets/simplify.py
@@ -0,0 +1,16 @@
+import sympy
+
+from mathsnuggets.core import fields, form
+
+test = {"expression": "x^2 - 5x + 6"}
+
+
+class Simplify(form.Form):
+    """Factorise"""
+
+    expression = fields.Expression("Expression")
+    template = "Simplify `expression` `simplify`"
+
+    @fields.computed("Solution")
+    def simplify(self):
+        return sympy.simplify(self.expression)
