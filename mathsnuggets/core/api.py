4bf372ed28849ad8ab52c7aa71fe9a431efe7ae4
Khoi Nguyen <khoi@nguyen.me.uk>
2021-03-13 17:00:41 +0100

    [API] Exclude whiteboard from slideshow

--- mathsnuggets/core/api.py
+++ mathsnuggets/core/api.py
@@ -108,7 +108,11 @@ def list_slideshows():
 @cache.memoize()
 def load_slideshow(teacher=False, year=False, slug=False):
     params = {"teacher": teacher, "year": year, "slug": slug}
-    return flask.jsonify(models.Slideshow(**params).children)
+    slideshow = models.Slideshow(**params).children
+    for slide in slideshow:
+        if "payload" in slide and "canvas" in slide["payload"]:
+            del slide["payload"]["canvas"]
+    return flask.jsonify(slideshow)
 
 
 @api.route("/slideshows/", methods=["POST"])
