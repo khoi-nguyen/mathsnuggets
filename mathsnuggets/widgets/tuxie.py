9919951520ee7672a94ab31955e2523457471429
Khoi Nguyen <khoi@nguyen.me.uk>
2021-05-31 18:14:52 +0200

    [WEB/Tuxie] Use overlay

--- mathsnuggets/widgets/tuxie.py
+++ mathsnuggets/widgets/tuxie.py
@@ -11,9 +11,9 @@ class Tuxie(form.Form):
     )
 
     template = """
-        <div v-if="config.edit">
-            `image`
-            Width: `width`
-        </div>
+        <widget-settings v-if="config.edit">
+            <config-option name="Image">`image`</config-option>
+            <config-option name="Width">`width`</config-option>
+        </widget-settings>
         <tuxie :width="payload.width" :image="payload.image">`text`</tuxie>
     """
