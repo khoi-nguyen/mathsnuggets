25f963b34c38c3ef1f545b54ebcefd629c73b5d2
Khoi Nguyen <khoi@nguyen.me.uk>
2021-07-07 15:08:07 +0200

    [WEB] Icon for environments

--- src/Environment.vue
+++ src/Environment.vue
@@ -1,20 +1,31 @@
 <template lang="pug">
 .container.avoid-column
+  widget-settings(v-if="config.edit")
+    config-option(name="Icon")
+      form-field(:editable="true" v-model="payload.icon" type="Field")
   b-collapse.message.is-medium(:class="`is-${payload.style || 'primary'}`" :open="!payload.collapsed")
     h3.message-header(slot="trigger")
-      form-field(:editable="config.edit" v-model="payload.title" type="Markdown")
+      .columns
+        .column.is-narrow(v-if="payload.icon")
+          b-icon(pack="fas" :icon="payload.icon" :key="payload.icon")
+        .column
+          form-field(:editable="config.edit" v-model="payload.title" type="Markdown")
     article
       .message-body
         slot
 </template>
 
 <script>
+import ConfigOption from './ConfigOption'
 import FormField from './FormField'
+import WidgetSettings from './WidgetSettings'
 
 export default {
   props: { config: Object, payload: { type: Object, default: () => {} } },
   components: {
-    FormField
+    ConfigOption,
+    FormField,
+    WidgetSettings
   }
 }
 </script>
--- src/WidgetSettings.vue
+++ src/WidgetSettings.vue
@@ -13,6 +13,6 @@ export default {
   position: absolute;
   top: 0;
   right: 0;
-  z-index: 1
+  z-index: 99;
 }
 </style>
