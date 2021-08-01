bf3510b3a190ebbb5e0212aa0bda80a9133bff63
Khoi Nguyen <khoi@nguyen.me.uk>
2021-08-01 20:57:22 +0200

    [WIDGETS] Merge both factorise widgets

--- mathsnuggets/widgets/factorise.py
+++ mathsnuggets/widgets/factorise.py
@@ -5,17 +5,44 @@ from mathsnuggets.core import fields, form
 test = {"expression": "x^2 - 5x + 6"}
 
 
-class Factorise(form.Form):
+class Factorise(form.MarkedForm):
     """Factorise"""
 
+    answer = fields.Expression("Your answer", nosave=True, editable=True)
     expression = fields.Expression("Expression")
-    template = "Factorise `expression` `factorise`"
+    marked_question = fields.Boolean("Marked question", default=False)
+    template = """
+        <widget-settings>
+            <config-option name="Marked Question">`marked_question`</config-option>
+        </widget-settings>
+        <div>Factorise `expression`</div>
+        <div v-if="!payload.marked_question">`solution`</div>
+        <survey :name="payload.name"
+            :correct="computed.correct"
+            :value="payload.answer" v-else>
+            Your answer: `answer`
+        </survey>
+    """
 
     def validate(self):
         self.expression = sympy.expand(self.expression)
+        super().validate()
 
-    @fields.computed("Factorised")
-    def factorise(self):
+    @fields.computed("Correct", field=fields.Boolean)
+    def correct(self):
+        if (
+            not self.answer
+            or sympy.simplify(self.answer - self.expression) != 0
+            or self.answer.func != sympy.Mul
+        ):
+            return False
+        for term in self.answer.args:
+            if sympy.factor(term).func == sympy.Mul:
+                return False
+        return True
+
+    @fields.computed("Solution")
+    def solution(self):
         return sympy.factor(self.expression)
 
     a = fields.RandomNumber("a")
