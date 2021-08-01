0574e1bccb6fa1816ad274d98172dd5599d57959
Khoi Nguyen <khoi@nguyen.me.uk>
2021-08-01 06:00:25 +0200

    [WIDGETS] Improve circle equation widget

--- mathsnuggets/widgets/circle_equation.py
+++ mathsnuggets/widgets/circle_equation.py
@@ -7,14 +7,22 @@ test = {"info": "radius", "equation": "x^2 + y^2 = 9"}
 
 
 class CircleEquation(form.Form):
-    """Circle radius and center"""
+    """Circle radius and centre"""
 
-    template = "Find the `info` of the circle `equation` `solution`"
+    template = """
+        Find the `info` of the circle whose equation is `equation`
+        <div v-if="payload.info === 'radius'">
+            `radius`
+        </div>
+        <div v-else>
+            `centre`
+        </div>
+    """
 
     equation = fields.Equation("Equation", required=True)
-    info = fields.Select("Info", options=["radius", "center"])
+    info = fields.Select("Info", options=["radius", "centre"])
 
-    @fields.computed("Radius/Center")
+    @fields.computed("Solution")
     def solution(self):
         (a, h, k, x, y), r = (
             sympy.symbols("a h k x y"),
@@ -27,9 +35,20 @@ class CircleEquation(form.Form):
         if not values:
             raise ValueError(f"{repr(self.equation)} is not an equation for a circle")
         values = values[0]
-        return values[r] if self.info == "radius" else (values[h], values[k])
+        return {"radius": values[r], "centre": (values[h], values[k])}
+
+    @fields.computed("Centre")
+    def centre(self):
+        return self.solution["centre"]
+
+    @fields.computed("Radius")
+    def radius(self):
+        return self.solution["radius"]
 
 
 def test_circle_equation():
     with pytest.raises(ValueError):
         CircleEquation(equation="x^2 + 1").solution
+
+    assert CircleEquation(equation="x^2 + y^2 = 9").radius == 3
+    assert CircleEquation(equation="(x-3)^2 + (y-2)^2 = 4").centre == (3, 2)
