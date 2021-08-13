2b12c94b2625f77ca75d2f6794e077ca15a3705b
Khoi Nguyen <khoi@nguyen.me.uk>
2021-08-13 15:34:16 +0200

    [WIDGETS] Add bar chart

---
+++ mathsnuggets/widgets/bar_chart.py
@@ -0,0 +1,24 @@
+from matplotlib import pyplot
+import sympy
+
+from mathsnuggets.core import fields, form
+
+
+class BarChart(form.Form):
+    """Bar Chart"""
+    data = fields.CSVData("Data", required=True)
+    size = fields.Expression("Figure size", default="6.4", numeric=True)
+    template = """
+        <widget-settings>
+            <config-option name="Data">`data`</config-option>
+            <config-option name="Size">`size`</config-option>
+        </widget-settings>
+        `bar_chart`
+    """
+
+    @fields.computed("Bar chart", field=fields.Html, nohide=True)
+    @fields.figure
+    def bar_chart(self):
+        fig, ax = pyplot.subplots()
+        ax.bar(self.data[0], self.data[1])
+        fig.set_size_inches(self.size, self.size)
