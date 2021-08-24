b6e2dc627e4a5972fd2a51d7cf15432226883337
Khoi Nguyen <khoi@nguyen.me.uk>
2021-08-24 20:53:58 +0200

    [WEB] Add Bacon le cochon

--- src/SpeechBubble.vue
+++ src/SpeechBubble.vue
@@ -27,7 +27,7 @@ export default {
   props: { payload: { type: Object, default: () => {} } },
   data () {
     return {
-      images: ['tuxie', 'jigglypuff', 'pikachu']
+      images: ['tuxie', 'jigglypuff', 'pikachu', 'bacon']
     }
   },
   components: {
