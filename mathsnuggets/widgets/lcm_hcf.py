653fa7f9b61d5e1c304985e23e3973b0d7a8303e
Khoi Nguyen <khoi@nguyen.me.uk>
2021-09-19 17:50:21 +0200

    [WIDGETS] Add generator for LCM and HCF

--- mathsnuggets/widgets/lcm_hcf.py
+++ mathsnuggets/widgets/lcm_hcf.py
@@ -30,6 +30,40 @@ class LcmHcf(form.MarkedForm):
         else:
             return tools.isequal(self.answer, sympy.lcm(self.numbers), 0)
 
+    a = fields.RandomNumber("a", default="0, 4")
+    b = fields.RandomNumber("b", default="0, 4")
+    c = fields.RandomNumber("c", default="0, 2")
+    d = fields.RandomNumber("d", default="0, 4")
+    e = fields.RandomNumber("e", default="0, 4")
+    f = fields.RandomNumber("f", default="0, 2")
+    exercise_type = fields.RandomNumber("t", default="0, 1")
+
+    def generator(self):
+        self.sought_quantity = (
+            "lowest common multiple" if self.exercise_type else "highest common factor"
+        )
+        self.numbers = [self.first_number, self.second_number]
+
+    @property
+    def first_number(self):
+        return 2 ** self.a * 3 ** self.b * 5 ** self.c
+
+    @property
+    def second_number(self):
+        return 2 ** self.d * 3 ** self.e * 5 ** self.f
+
+    @fields.constraint("Distinct", default=True, hidden=True, protected=True)
+    def distinct(self):
+        return self.first_number != self.second_number
+
+    @fields.constraint("Numbers are smaller than 1000", default=True)
+    def small_enough(self):
+        return self.first_number < 1000 and self.second_number < 1000
+
+    @fields.constraint("Numbers are smaller than 100")
+    def small(self):
+        return self.first_number < 100 and self.second_number < 100
+
 
 def test_lcm_hcf():
     lcm, hcf = "lowest common multiple", "highest common factor"
