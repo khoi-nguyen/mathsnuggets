205a9fd0d31728fcd88726ec5e92ab674c45154b
Khoi Nguyen <khoi@nguyen.me.uk>
2022-05-23 20:41:48 +0200

    [CORE] Fix missing doit attribute bug

--- mathsnuggets/core/tools.py
+++ mathsnuggets/core/tools.py
@@ -2,7 +2,11 @@ import sympy
 
 
 def isequal(expr1, expr2, error=0):
-    expr = sympy.Abs(sympy.nsimplify(expr1.doit()) - sympy.nsimplify(expr2.doit()))
+    if hasattr(expr1, "doit"):
+        expr1 = expr1.doit()
+    if hasattr(expr2, "doit"):
+        expr2 = expr2.doit()
+    expr = sympy.Abs(sympy.nsimplify(expr1) - sympy.nsimplify(expr2))
     if len(expr.atoms(sympy.Symbol)):
         return False
     return expr <= error
