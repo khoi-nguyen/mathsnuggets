c1c58d0c303fc42a5a3de4997d302179006a4d76
Khoi Nguyen <khoi@nguyen.me.uk>
2022-12-03 05:33:47 +0100

    Undo previous commit

--- mathsnuggets/core/api.py
+++ mathsnuggets/core/api.py
@@ -97,7 +97,7 @@ def join(survey):
 
 
 @api.route("/slideshows", methods=["GET"])
-@cache.memoize(timeout=0)
+@cache.memoize()
 def list_slideshows():
     return flask.jsonify([dict(s) for s in models.Slideshow.find({})])
 
